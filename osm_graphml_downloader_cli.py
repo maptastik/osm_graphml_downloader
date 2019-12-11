24f06b4c31268e94fb1c21604cc312fbc27f3e80
Ryan Cooper <ryancooper729@gmail.com>
2019-12-11 11:04:01 -0500

    Add additional help docs and clean up data types

--- osm_graphml_downloader_cli.py
+++ osm_graphml_downloader_cli.py
@@ -8,20 +8,20 @@ from osm_graphml_downloader import osm_graphml_downloader
 @click.command()
 @click.option('-x', '--network_type', help = 'Network type: "walk", "bike", "drive", "drive_service", "all", "all_private", "none"')
 @click.option('-o', '--out_dir', help = 'Path to directory where you want the results to go')
-@click.option('-f', '--filename', default = None)
-@click.option('-n', '--north')
-@click.option('-s', '--south')
-@click.option('-e', '--east')
-@click.option('-w', '--west')
+@click.option('-f', '--filename', default = None, help = 'Name of .graphml ouput file')
+@click.option('-n', '--north', help = 'Area of interest north extent', type = float)
+@click.option('-s', '--south', help = 'Area of interest south extent', type = float)
+@click.option('-e', '--east', help = 'Area of interest east extent', type = float)
+@click.option('-w', '--west', help = 'Area of interest west extent', type = float)
 @click.option('--reproject', is_flag = True, help = 'Option to reproject the graph from WGS84 to something else')
 @click.option('--epsg_code', help = 'EPSG code value. Only used if --reproject flag is used', default = 2264)
 def main(network_type, out_dir, filename, north, south, east, west, reproject, epsg_code):
     osm_graphml_downloader(network_type = network_type,
                          out_dir = out_dir,
                          filename = filename,
-                         bbox = [float(west), float(south), float(east), float(north)],
+                         bbox = [west, south, east, north],
                          reproject = reproject,
-                         epsg_code = float(epsg_code))
+                         epsg_code = int(epsg_code))
 
     end = perf_counter()
     completion_seconds = end - start
